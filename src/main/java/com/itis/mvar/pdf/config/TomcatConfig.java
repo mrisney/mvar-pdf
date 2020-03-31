dd36db8290d22b2eb98a38f0a076ac0a37f34f47
Marc Risney <marc.risney@gmail.com>
2020-03-30 20:27:26 -0700

    update

--- src/main/java/com/itis/mvar/pdf/config/TomcatConfig.java
+++ src/main/java/com/itis/mvar/pdf/config/TomcatConfig.java
@@ -1,7 +1,7 @@
 package com.itis.mvar.pdf.config;
 
 import org.apache.catalina.connector.Connector;
-import org.apache.catalina.valves.RemoteIpValve;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,34 +10,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatConfig {
 
-	@Value("${ajp.port}")
-	int ajpPort;
 
-	@Value("${ajp.enabled}")
-	boolean ajpEnabled;
+    @Value("${ajp.port}")
+    int ajpPort;
 
-	private static RemoteIpValve createRemoteIpValves() {
-		RemoteIpValve remoteIpValve = new RemoteIpValve();
-		remoteIpValve.setRemoteIpHeader("x-forwarded-for");
-		remoteIpValve.setProtocolHeader("x-forwarded-proto");
-		return remoteIpValve;
-	}
+    @Value("${ajp.remoteauthentication}")
+    String remoteAuthentication;
 
-	@Bean
-	@SuppressWarnings("static-method")
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-		tomcat.addAdditionalTomcatConnectors(createConnector());
-		tomcat.addContextValves(createRemoteIpValves());
-		return tomcat;
-	}
+    @Value("${ajp.enabled}")
+    boolean tomcatAjpEnabled;
 
-	private Connector createConnector() {
-		Connector connector = new Connector("AJP/1.3");
-		connector.setPort(ajpPort);
-		connector.setSecure(false);
-		connector.setScheme("http");
-		connector.setAllowTrace(false);
-		return connector;
-	}
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        if (tomcatAjpEnabled) {
+            Connector ajpConnector = new Connector("AJP/1.3");
+            ajpConnector.setPort(ajpPort);
+            ajpConnector.setSecure(false);
+            ajpConnector.setAllowTrace(false);
+            ajpConnector.setScheme("http");
+            ((AbstractAjpProtocol) ajpConnector.getProtocolHandler()).setSecretRequired(false);
+            tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        }
+
+        return tomcat;
+    }
+
 }
