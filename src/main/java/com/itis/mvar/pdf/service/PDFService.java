6bd76069b5312c8e8bde98327c9f87ba24853021
Marc Risney <marc.risney@gmail.com>
2020-04-01 21:46:42 -0700

    update

--- src/main/java/com/itis/mvar/pdf/service/PDFService.java
+++ src/main/java/com/itis/mvar/pdf/service/PDFService.java
@@ -1,11 +1,15 @@
 package com.itis.mvar.pdf.service;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.apache.pdfbox.io.MemoryUsageSetting;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
@@ -29,20 +33,17 @@ public class PDFService {
 	public void merge(int crashId, Schema schema) throws IOException {
 
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		
+
 		try {
 			PDFMergerUtility mergePdf = new PDFMergerUtility();
 
 			mergePdf.setDestinationStream(baos);
-			List<MVARReportInput> mvarReports = reportDAO.getMVARReportInputs(crashId,schema.toText());
+			List<MVARReportInput> mvarReports = reportDAO.getMVARReportInputs(crashId, schema.toText());
 			mergePdf.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
 			byte[] reportBytes = baos.toByteArray();
 
-			MVARReportOutput outputReport = MVARReportOutput.builder()
-					.crashId(crashId)
-					.reportMime("application/pdf;charset=UTF-8")
-					.totalPages(mvarReports.size())
-					.reportBytes(reportBytes)
+			MVARReportOutput outputReport = MVARReportOutput.builder().crashId(crashId)
+					.reportMime("application/pdf;charset=UTF-8").totalPages(mvarReports.size()).reportBytes(reportBytes)
 					.build();
 
 			MVARReportDAO.insertMVARReportOutput(outputReport);
@@ -57,7 +58,7 @@ public class PDFService {
 	public ByteArrayOutputStream download(Integer crashId, String agency) throws IOException {
 
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		
+
 		try {
 			PDFMergerUtility mergePdf = new PDFMergerUtility();
 			mergePdf.setDestinationStream(baos);
@@ -66,8 +67,14 @@ public class PDFService {
 
 			List<InputStream> sourcesList = new LinkedList<InputStream>();
 			for (MVARReportInput report : reports) {
+			
+				
 				InputStream is = new ByteArrayInputStream(report.getReportBytes());
-				sourcesList.add(is);
+				// convert byte array back to BufferedImage
+				BufferedImage bImageFromConvert = ImageIO.read(is);
+				if (!isBlank(bImageFromConvert)) {
+					sourcesList.add(is);
+				}
 			}
 			mergePdf.addSources(sourcesList);
 			mergePdf.mergeDocuments(MemoryUsageSetting.setupTempFileOnly());
@@ -79,5 +86,26 @@ public class PDFService {
 			log.error(e.toString());
 		}
 		return baos;
+	}
+
+	private static Boolean isBlank(BufferedImage pageImage) throws IOException {
+		BufferedImage bufferedImage = pageImage;
+		long count = 0;
+		int height = bufferedImage.getHeight();
+		int width = bufferedImage.getWidth();
+		Double areaFactor = (width * height) * 0.99;
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				Color c = new Color(bufferedImage.getRGB(x, y));
+				if (c.getRed() == c.getGreen() && c.getRed() == c.getBlue() && c.getRed() >= 248) {
+					count++;
+				}
+			}
+		}
+		if (count >= areaFactor) {
+			return true;
+		}
+		return false;
 	}
 }
